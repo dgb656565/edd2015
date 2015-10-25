8ccc25d5ba23fe8cf343dc45c8ea53f984d3714c
dgb656565 <tu_656565@hotmail.com>
2015-10-25 15:27:27 -0600

    Funciones principales

--- calculadora/funciones.c
+++ calculadora/funciones.c
@@ -7,3 +7,20 @@ float suma (float num1, float num2)
 }
 
 
+float resta (float num1, float num2) 
+{
+        return  num1 - num2;
+}
+
+
+float multiplicacion (float num1, float num2) 
+{
+        return  num1 * num2;
+}
+
+
+float division (float num1, float num2) 
+{
+        return  num1 / num2;
+}
+
