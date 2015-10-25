23040622268bb80b3f6fbff77b4e97a5438445d1
dgb656565 <tu_656565@hotmail.com>
2015-10-25 15:28:31 -0600

    Agregar funciones faltantes a la calculadora

--- calculadora/main.c
+++ calculadora/main.c
@@ -51,10 +51,19 @@ int main()
 				imprimir_resultado(resultado);
 				break;
 			case RESTA:
+                                introducir_datos(&numero1, &numero2);
+                                resultado = resta(numero1, numero2);
+                                imprimir_resultado(resultado);
 				break;
 			case MULTIPLICACION:
+                                introducir_datos(&numero1, &numero2);
+                                resultado = multiplicacion(numero1, numero2);
+                                imprimir_resultado(resultado);
 				break;
 			case DIVISION:
+                                introducir_datos(&numero1, &numero2);
+                                resultado = division(numero1, numero2);
+                                imprimir_resultado(resultado);
 				break;
 			default:
 				break;
