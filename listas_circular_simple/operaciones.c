e14fe21dbe67b7a747a86d9c553c9b262eed2144
David González B <tu_656565@hotmail.com>
2015-11-05 17:56:39 +0000

    Arreglar funcion borrar_elemento

--- listas_circular_simple/operaciones.c
+++ listas_circular_simple/operaciones.c
@@ -84,24 +84,17 @@ void borrar_lista(struct nodo *cabecera)
 
 void borrar_elemento(struct nodo *cabecera, int valor_borrar)
 {
-	struct nodo *actual;
-	struct nodo *borrar;
+    struct nodo *borrar;
+    struct nodo *actual;
 
-	actual = cabecera;
-	borrar = cabecera;
-
-	borrar->val = valor_borrar;
-
-	while(actual->sig != cabecera){
-		borrar = borrar->sig;
-		if(borrar->val == valor_borrar){
-			actual->sig = borrar->sig;
-			free(borrar);
-		}
-		actual=actual->sig;
-	}
+    actual=cabecera;
+    
+    if(actual->sig != cabecera){
+    	while (actual->sig->val != valor_borrar){
+        	actual=actual->sig;
+    	}
+            borrar = actual->sig;
+            actual->sig = borrar->sig;
+            free(borrar);
+    }
 }
-
-
-
-
