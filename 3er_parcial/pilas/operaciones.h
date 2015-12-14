b37555de4b1672e091f7444ed6bafc7a88c9e0ad
dgb656565 <tu_656565@hotmail.com>
2015-12-13 18:46:15 -0600

    Agregar operaciones de la pila

---
+++ 3er_parcial/pilas/operaciones.h
@@ -0,0 +1,14 @@
+#ifndef _OPERACIONES_H_
+#define _OPERACIONES_H_
+
+#include "nodo.h"
+
+void inicializar(struct nodo *elemento);
+
+void push(struct nodo *elemento, int valor_nuevo);
+
+void tope(struct nodo *elemento);
+
+void pop(struct nodo *elemento);
+
+#endif
